2e8baf85f8f90c28906af4a7fd1c63c3235dfdc1
Fred <contact@brocorp.re>
2021-08-28 09:37:59 +0400

    Update

--- components/common/UpdateNotice.js
+++ components/common/UpdateNotice.js
@@ -30,7 +30,7 @@ export default function UpdateNotice() {
       <div className={styles.message}>
         <FormattedMessage
           id="message.new-version-available"
-          defaultMessage="A new version of brostats {version} is available!"
+          defaultMessage="A new version of umami {version} is available!"
           values={{ version: `v${latest}` }}
         />
       </div>
